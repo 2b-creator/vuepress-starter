f1c7ff0aa11333cbbd7d9c95439cf6be2107951d
2b-creator <qjtykr65536@gmail.com>
2025-03-22 03:46:55 +0000

    cm

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -13,7 +13,7 @@ export default hopeTheme({
 
   logo: "https://theme-hope-assets.vuejs.press/logo.svg",
 
-  repo: "vuepress-theme-hope/vuepress-theme-hope",
+  repo: "2b-creator/vuepress-starter",
 
   docsDir: "src",
 
